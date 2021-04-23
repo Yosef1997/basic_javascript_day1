b20fe91da11a14b56a8c51d8fae63a28f5789b50
unknown <yosefsitumorang97@gmail.com>
2021-04-23 21:12:31 +0700

    cs week exercise

---
+++ latihan3.js
@@ -0,0 +1,105 @@
+// // Flowgorithm Palindrom
+// const palindrom = (data) => {
+//   if(typeof(data) === 'string'){
+//       let newArr = data.split().reverse().join('')
+//       if(data === newArr){
+//           return console.log(`${data} adalah palindrom`)
+//       }else{
+//           return console.log(`${data} adalah non-palindrom`)
+//       }
+//   }else{
+//       return `Please use parameter function with string data`
+//   }
+// }
+
+// palindrom('makam')
+
+// Flowgorithm Reverse Word
+
+// const reverseStr = (string) => {
+//   let result=null
+//   if(typeof string === 'string'){
+//     result=string.split('').reverse().join('')
+//   } else {
+//     result='Please input string data'
+//   }
+//   return console.log(result)
+// }
+
+// reverseStr('Saya belajar Javascript di arkademy')
+
+// // Flowgorithm Reverse Word
+
+const checkParagraph = (sentence) => {
+    const data = sentence.split(' ')
+    const paragraph = ['saya', 'pergi', 'ke', 'kantor', 'hari', 'kamis']
+    let result =[]
+    if (typeof sentence === 'string' && data.length >= 2) {
+       for(let i = 0; i < data.length; i++){
+           for(let j = 0; j < paragraph.length; j++ ){
+               if(data[i]===paragraph[j]){
+                   result.push(data[i] + ' ')
+               }
+           }
+       }
+       if (data.length === result.length){
+        return console.log('true')
+      } else {
+        return console.log('false')
+      }
+    } else {
+        return console.log('Please input string data and have at least 3 words')
+    }
+}
+
+console.log(checkParagraph('jalan kantor'))
+
+// // Flowgorithm Mencari angka terbesar
+
+// const maxData = (num) => {
+//   const data = num
+//   if (Array.isArray(data) === Array.isArray([])){
+//     const results = data.sort(function(a,b){return a-b})
+//     return console.log(results[-1])
+//   }else {
+//     return console.log('Wrong input')
+//   }
+
+// }
+
+// maxData([1,5,2])
+
+// // Flowgorithm Ascendents number
+
+// const maxData = (num) => {
+//   const data = num
+//   if (Array.isArray(data) === Array.isArray([])){
+//     const results = data.sort(function(a,b){return a-b})
+//     return console.log(results)
+//   }else {
+//     return console.log('Wrong input')
+//   }
+
+// }
+
+// maxData([1,5,2])
+
+// // Flowgorithm Deret Angka Yang Sama Antara 2 Array
+
+// const checkParagraph = (arr1, arr2) => {
+//     const array1 = arr1
+//     const array2 = arr2
+//     let result = [] 
+//     if (Array.isArray(array1) === Array.isArray([]) && Array.isArray(array2) === Array.isArray([])) {
+//        for(let i = 0; i < array1.length; i++){
+//            for(let j = 0; j < array2.length; j++ ){
+//                if(array1[i]===array2[j]){
+//                    result.push(array[i])
+//                }
+//            }
+//        }
+//        return console.log(result)
+//     } else {
+//         return console.log('Please input array data')
+//     }
+// }
--- task1.js
+++ task1.js
@@ -1,28 +1,26 @@
-const palindromCheck = (string) => {
-    if(typeof(string) === 'string'){
-        let newString = string.toLowerCase().replace(/\W/g,'')
-        let length = newString.length
-        for(let index = 0; index < length; index++){
-            if(newString[index] === newString[length - 1 - index]){
-                return console.log(`${string} adalah palindrom`)
-            }else{
-                return console.log(`${string} adalah non-palinfrom`)
-            }
-        }
-    }else{
-        console.log(`Please use parameter function with string data`)
+// const palindromCheck = (string) => {
+//     if(typeof(string) === 'string'){
+//         let newString = string.toLowerCase().replace(/\W/g,'')
+//         let length = newString.length
+//         for(let index = 0; index < length; index++){
+//             if(newString[index] === newString[length - 1 - index]){
+//                 return console.log(`${string} adalah palindrom`)
+//             }else{
+//                 return console.log(`${string} adalah non-palinfrom`)
+//             }
+//         }
+//     }else{
+//         console.log(`Please use parameter function with string data`)
         
-    }
+//     }
     
-}
-palindromCheck('isiasdasd')
+// }
+// palindromCheck('isiasdasd')
 
 const palindrom = (data) => {
     if(typeof(data) === 'string'){
-        let newData = data.toLowerCase().replace(/\W/g,'')
-        let array = newData.split('')
-        let newArr = array.reverse().join('')
-        if(newData === newArr){
+        let newArr = data.split().reverse().join('')
+        if(data === newArr){
             return console.log(`${data} adalah palindrom`)
         }else{
             return console.log(`${data} adalah non-palindrom`)
@@ -32,4 +30,31 @@ const palindrom = (data) => {
     }
 }
 
-palindrom('maling')
+palindrom('makam')
+// const num =789869
+// console.log(num.toString().split(''))
+// const palindrom = 'makas'
+// console.log(palindrom.split('').reverse())
+// const paragraph = 'Pada suatu hari, saya pergi ke kantor. Ternyata kantor saya hari ini tutup.'
+// const newParagraph = paragraph.split(' ')
+// const length = newParagraph.length
+
+// const checkParagraph = (sentence) => {
+//     const data = sentence.split(' ')
+//     const dataLength = data.length
+//     let result = null 
+//     if (typeof sentence === 'string' && dataLength > 2) {
+//        for(let i = 0; i < dataLength; i++){
+//            for(let j = 0; j < length; j++ ){
+//                if(data[i]===newParagraph[j]){
+//                    result= data[i]
+//                }
+//            }
+//        }
+//        return result
+//     } else {
+//         return console.log('Please input string data and have at least 3 words')
+//     }
+// }
+
+// checkParagraph('saya pergi kantor')
